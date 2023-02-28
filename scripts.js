07f8709bd9a228c9a7f81261aca71c5d9c40b510
Ambrosius <ambrosius411@protonmail.com>
2023-02-28 11:38:51 +0100

    Add javascript functions for calculate  operators

--- scripts.js
+++ scripts.js
@@ -1,1 +1,31 @@
-console.log('hello')
+function add(a,b) {
+    return a + b
+}
+
+function subtract(a,b) {
+    return a - b
+}
+
+function multiply(a,b) {
+    return a * b
+}
+
+function divide(a,b) {
+    return a / b
+}
+
+function operate(operator, a, b){
+    switch (operator) {
+        case '+':
+            return add(a,b)
+        case '-':
+            return subtract(a,b)
+        case 'x':
+        case '*':
+            return multiply(a,b)
+        case '/':
+            return divide(a,b)
+        default:
+            throw "Wrong operator input"
+    }
+}
