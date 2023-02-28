fa663db01bc1161f7ea2663261627c6eb143813d
Ambrosius <ambrosius411@protonmail.com>
2023-02-28 14:03:03 +0100

    Adds listeners so operations are performed on button presses

--- scripts.js
+++ scripts.js
@@ -1,45 +1,81 @@
 let displayValue = '';
+let operator = ''
+resultValue = 0;
+let resultButtonPressedLast = false    //Changes functionally based on whether the `=` button was pressed last
 
 const display = document.getElementById('display');
 document.querySelectorAll('.number').forEach(item => {
     item.addEventListener('click', inputNumber)
 })
 
+document.querySelectorAll('.operator').forEach(item => {
+    item.addEventListener('click', inputOperator)
+})
+
+document.querySelectorAll('.result').forEach(item => {
+    item.addEventListener('click', calculateResult)
+})
+
 function inputNumber(e) {
+    if (resultButtonPressedLast) {
+        displayValue = '';
+        resultValue = 0;
+        resultButtonPressedLast = false
+    }
     let value = e.target.value
-    console.log(value)
     displayValue = displayValue + value
-    display.innerText = displayValue 
+    display.innerText = displayValue
 }
 
+function inputOperator(e) {
+    operator = e.target.value
+    if (!resultButtonPressedLast) {
+        if (resultValue === 0) {
+            resultValue = parseInt(displayValue)
+        }
+        else {
+            resultValue = operate(operator, resultValue, parseInt(displayValue))
+        }
+    }
+    else {
+        resultButtonPressedLast = false
+    }
+    displayValue = '';
+}
 
-function add(a,b) {
+function calculateResult(e) {
+    resultValue = operate(operator, resultValue, parseInt(displayValue))
+    display.innerText = resultValue
+    resultButtonPressedLast = true
+}
+
+function add(a, b) {
     return a + b
 }
 
-function subtract(a,b) {
+function subtract(a, b) {
     return a - b
 }
 
-function multiply(a,b) {
+function multiply(a, b) {
     return a * b
 }
 
-function divide(a,b) {
+function divide(a, b) {
     return a / b
 }
 
-function operate(operator, a, b){
+function operate(operator, a, b) {
     switch (operator) {
         case '+':
-            return add(a,b)
+            return add(a, b)
         case '-':
-            return subtract(a,b)
+            return subtract(a, b)
         case 'x':
         case '*':
-            return multiply(a,b)
+            return multiply(a, b)
         case '/':
-            return divide(a,b)
+            return divide(a, b)
         default:
             throw "Wrong operator input"
     }
